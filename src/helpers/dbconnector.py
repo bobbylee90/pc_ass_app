3a1311750fd46d9d583aeeb17b7519094d96b084
bobbylee90 <mfbobbylee@gmail.com>
2023-10-18 07:35:31 -0700

    adding more information for reference

--- src/helpers/dbconnector.py
+++ src/helpers/dbconnector.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-
+import sqlalchemy
 
 class MysqlConnector():
 
@@ -9,6 +9,17 @@ class MysqlConnector():
         self.db_host = "mysql"
         self.db_port = 3306
         self.db_name = "default"
+        self.template_engine = "mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+        self.engine = self.get_engine()
+
+    def get_engine(self):
+        eg_conf = self.template_engine.format(db_username=self.username,
+            db_password=self.password,
+            db_host=self.db_host,
+            db_port=self.db_port,
+            db_name =self.db_name)
+        return create_engine(eg_conf, echo=True)
+
 
     def __enter__(self):
         return self
